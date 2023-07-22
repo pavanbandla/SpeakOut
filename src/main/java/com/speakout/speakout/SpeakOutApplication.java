caa17f886e46be476d397130e050a6187f0b42bc
pavanbandla <pavankrishnashivu@gmail.com>
2023-07-22 11:25:34 -0400

    Initial commit

---
+++ src/main/java/com/speakout/speakout/SpeakOutApplication.java
@@ -0,0 +1,13 @@
+package com.speakout.speakout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakOutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/speakout/speakout/SpeakOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speakout.speakout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
